f1264d47dcead9dccda1194e973ae2ca8a7b24ce
LeoBTan <99303598+LeoBTan@users.noreply.github.com>
2026-02-02 22:45:30 -0500

    Added more files for fun

---
+++ FUN.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class FUN {
+    public static void main(String[] args) {
+       double number = 16;
+       System.out.println(number);
+    }
+}
---
+++ RandomNumber.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class RandomNumber {
+    public static void main(String[] args) {
+        System.out.println("Min:");
+        Scanner min = new Scanner(System.in);
+        int Min = min.nextInt();
+        System.out.println("Max:");
+        Scanner max = new Scanner(System.in);
+        int Max = max.nextInt();
+        int random = (Min + (int)(Math.random() * ((Max - Min) + 1)));
+        System.out.println(random);
+    }
+}

7385d9ebe63c840f63ecf567a1d2cdb979c44321
LeoBTan <leobtan@icloud.com>
2026-02-02 21:51:13 -0500

    added comments to the code and javadocs to the class

--- src/dmoj/TimeToDecompress.java
+++ src/dmoj/TimeToDecompress.java
@@ -12,15 +12,15 @@ public class TimeToDecompress {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in); // Initialize scanner
 
-        int L = scan.nextInt(); // Get the first integer L, the number of lines in the message
+        int Lines = scan.nextInt(); // Get the first integer L, the number of lines in the message
 
-        for (int i = 0; i < L; i++) { // Create a for loop that loops until there are the same number of lines as stated in variable L
-            int nint = scan.nextInt(); // Get the integer that states how many times the character should be printed
+        for (int i = 0; i < Lines; i++) { // Create a for loop that loops until there are the same number of lines as stated in variable L
+            int numberOfRepeats = scan.nextInt(); // Get the integer that states how many times the character should be printed
 
-            String n = scan.nextLine().strip(); // Get the character from the line and remove any white space
+            String stringToRepeat = scan.nextLine().strip(); // Get the character from the line and remove any white space
 
-            for (int d = 0; d < nint; d++) { // Create a for loop to loop printing the same character
-                System.out.print(n); // Print the desired character
+            for (int d = 0; d < numberOfRepeats; d++) { // Create a for loop to loop printing the same character
+                System.out.print(stringToRepeat); // Print the desired character
             }
             System.out.println(""); // Add a new line for formatting
         }

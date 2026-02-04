339775381134eedbf4c10979c6357b38fc99ab4e
LeoBTan <leobtan@icloud.com>
2026-02-04 14:56:23 -0500

    code from the guided activity

---
+++ src/PrintWithLoops.java
@@ -0,0 +1,18 @@
+public class PrintWithLoops {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+        System.out.println();
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i+1);
+        }
+        System.out.println();
+
+        for (int i = 0; i < 12; i+= 2) {
+            System.out.println((int) (2 * Math.pow(i, 2) + 5));
+        }
+    }
+}

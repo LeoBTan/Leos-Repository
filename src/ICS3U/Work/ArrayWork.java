3d82ac5d1e5f8c90797d538a20a2a09ac2c22b59
LeoBTan <leobtan@icloud.com>
2026-02-20 10:09:15 -0500

    finished in-class activity

--- src/ICS3U/Work/ArrayWork.java
+++ src/ICS3U/Work/ArrayWork.java
@@ -2,6 +2,27 @@ package ICS3U.Work;
 
 public class ArrayWork {
     public static void main(String[] args) {
-        
+        int[] nums = {1, 3, 7, 2, 6};
+        int[] arr = {2, 6, 1, 8};
+
+        int largest = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > largest) {
+                largest = nums[i];
+            }
+        }
+
+        System.out.println(largest);
+
+        int largest2 = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > largest2) {
+                largest2 = arr[i];
+            }
+        }
+
+        System.out.println(largest2);
     }
 }

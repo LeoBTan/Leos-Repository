993931d9ee2c1b59fbfa9060561268ff5bd716d7
LeoBTan <leobtan@icloud.com>
2026-03-23 14:43:05 -0400

    in-class DMOJ

---
+++ src/ICS3U/DMOJ/J/J2/ModInverse.java
@@ -0,0 +1,25 @@
+package ICS3U.DMOJ.J.J2;
+
+import java.util.Scanner;
+
+public class ModInverse {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int x = scan.nextInt();
+        int m = scan.nextInt();
+        int n = 1;
+
+        while (x*n % m != 1) {
+            if (n > 100) {
+                break;
+            }
+            n++;
+        }
+
+        if (n > 100) {
+            System.out.println("No such integer exists.");
+        } else {
+            System.out.println(n);
+        }
+    }
+}

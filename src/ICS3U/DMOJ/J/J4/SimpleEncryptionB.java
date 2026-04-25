8b0bc8fedd999c4c4788fcc2b77ec2f6d005d1e0
LeoBTan <leobtan@icloud.com>
2026-04-25 19:38:29 -0400

    random commits

---
+++ src/ICS3U/DMOJ/J/J4/SimpleEncryptionB.java
@@ -0,0 +1,18 @@
+package ICS3U.DMOJ.J.J4;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class SimpleEncryptionB {
+    public static void main(String[] args) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+
+        String key = read.readLine();
+        String message = read.readLine().replaceAll("[^a-zA-Z]", "");
+
+        for (int i = 0; i < message.length(); i++) {
+            
+        }
+    }
+}

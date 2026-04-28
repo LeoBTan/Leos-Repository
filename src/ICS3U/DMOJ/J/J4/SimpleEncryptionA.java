16eb3a06bf0d198e3c7fadbba655036750c58b86
LeoBTan <leobtan@icloud.com>
2026-04-27 21:33:34 -0400

    comment simple encryption

--- src/ICS3U/DMOJ/J/J4/SimpleEncryptionA.java
+++ src/ICS3U/DMOJ/J/J4/SimpleEncryptionA.java
@@ -4,25 +4,29 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+// Simple Encryption first iteration.
 public class SimpleEncryptionA {
     public static void main(String[] args) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in)); // Initialze BufferedReader to read input.
 
-        String[] key = read.readLine().split("");
-        String message = read.readLine().replaceAll("[^a-zA-Z]", "");
-        String[] messageArray = message.split("");
+        String[] key = read.readLine().split(""); // Read the key and store it as an array of strings.
+        String message = read.readLine().replaceAll("[^a-zA-Z]", ""); // Read the message, remove all non-alphabetic characters, and store it as a string.
+        String[] messageArray = message.split(""); // Split the message into an array of strings.
 
+        // Loop through each character in the key
         for (int i = 0; i < key.length; i++) {
+            // Loop through each character in the message array, incrementing by the length of the key to get the proper key character for each message character.
             for (int j = i; j < messageArray.length; j+=key.length) {
-                int tempInt = messageArray[j].charAt(0) + (key[i].charAt(0) - 65);
+                int tempInt = messageArray[j].charAt(0) + (key[i].charAt(0) - 65); // Perform character arithmetic.
+                // Keep all values within the range of captial letters.
                 if (tempInt > 90) {
                     tempInt -= 26;
                 }
-                messageArray[j] = String.valueOf((char)tempInt);
+                messageArray[j] = String.valueOf((char)tempInt); // Convert the integer back to a character and store it in the message array.
             }
         }
         
-        String newString = String.join("", messageArray);
-        System.out.println(newString);
+        String newString = String.join("", messageArray); // Join the message array back into a string.
+        System.out.println(newString); // Print the encrypted message.
     }
 }
--- src/ICS3U/DMOJ/J/J4/SimpleEncryptionB.java
+++ src/ICS3U/DMOJ/J/J4/SimpleEncryptionB.java
@@ -4,19 +4,26 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
+/*
+ * Author: Leo Tan
+ * Date: Monday, April 27, 2026
+ * Problem: CCC '04 J4 - Simple Encryption
+ * Description: Using strings and .charAt() to perform character arithmetic to encrypt the message.
+ */
 public class SimpleEncryptionB {
     public static void main(String[] args) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in)); // Initialze BufferedReader to read input.
 
-        String key = read.readLine();
-        String message = read.readLine().replaceAll("[^a-zA-Z]", "");
+        String key = read.readLine(); // Read the key and store it as a string
+        String message = read.readLine().replaceAll("[^a-zA-Z]", ""); // Read the message, remove all non-alphabetic characters, and store it as a string.
 
-        int temp;
+        int temp; // Initialize a temporary variable to store the result of the character arithmetic.
 
+        // Loop through each character in the message
         for (int i = 0; i < message.length(); i++) {
-            temp = (message.charAt(i) + key.charAt(i % key.length()) - 65);
-            if (temp > 90) temp -=26;
-            System.out.print((char) temp);
+            temp = (message.charAt(i) + key.charAt(i % key.length()) - 65); // Perfrom character arithmetic with the proper key character adjustment.
+            if (temp > 90) temp -=26; // Keep all values within the range of captial letters.
+            System.out.print((char) temp); // Print the encrypted character without a newline to abide by the output format.
         }
     }
 }

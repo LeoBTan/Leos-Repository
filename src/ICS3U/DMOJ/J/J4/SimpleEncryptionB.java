d0357f73b96d4288d53101f6c6f113f51f4254ba
LeoBTan <leobtan@icloud.com>
2026-04-27 21:19:05 -0400

    finished second iteration of simple encryption

--- src/ICS3U/DMOJ/J/J4/SimpleEncryptionB.java
+++ src/ICS3U/DMOJ/J/J4/SimpleEncryptionB.java
@@ -11,7 +11,12 @@ public class SimpleEncryptionB {
         String key = read.readLine();
         String message = read.readLine().replaceAll("[^a-zA-Z]", "");
 
+        int temp;
+
         for (int i = 0; i < message.length(); i++) {
+            temp = (message.charAt(i) + key.charAt(i % key.length()) - 65);
+            if (temp > 90) temp -=26;
+            System.out.print((char) temp);
         }
     }
 }

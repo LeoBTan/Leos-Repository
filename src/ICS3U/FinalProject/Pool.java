a791f701d37cd35ea026088856a0032f155fd968
JackZ125 <jackzhou25@crescentschool.org>
2026-05-06 12:14:59 -0400

    Ball images and some are broken

--- src/ICS3U/FinalProject/Pool.java
+++ src/ICS3U/FinalProject/Pool.java
@@ -3,20 +3,74 @@ package ICS3U.FinalProject;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
 
 public class Pool extends JFrame {
+    private CardLayout cardLayout;
+    private JPanel mainPanel;
+
     public Pool() {
-        this.add(new GamePanel());
         this.setTitle("Pool");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
+        this.setLocationRelativeTo(null);
+
+        cardLayout = new CardLayout();
+        mainPanel = new JPanel(cardLayout);
+
+        LandingPage landingPage = new LandingPage(this);
+        GamePanel gamePanel = new GamePanel(this);
+
+        mainPanel.add(landingPage, "LANDING");
+        mainPanel.add(gamePanel, "GAME");
+
+        this.add(mainPanel);
         this.pack();
         this.setVisible(true);
-        this.setLocationRelativeTo(null);
+
+        cardLayout.show(mainPanel, "LANDING");
+    }
+
+    public void switchToGame() {
+        cardLayout.show(mainPanel, "GAME");
     }
 
     public static void main(String[] args) {
         new Pool();
+    }
+}
+
+class LandingPage extends JPanel {
+    private BufferedImage backgroundImage;
+    private JButton startButton;
+
+    LandingPage(Pool pool) {
+        this.setPreferredSize(new Dimension(800, 800));
+        this.setLayout(null);
+
+        // Load background image
+        try {
+            backgroundImage = new javax.imageio.ImageIO().read(
+                new java.io.File("src/ICS3U/FinalProject/pooltable.png")
+            );
+        } catch (Exception e) {
+            System.out.println("Image not found");
+        }
+
+        // Create start button
+        startButton = new JButton("START GAME");
+        startButton.setBounds(300, 350, 200, 100);
+        startButton.setFont(new Font("Arial", Font.BOLD, 20));
+        startButton.addActionListener(e -> pool.switchToGame());
+        this.add(startButton);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (backgroundImage != null) {
+            g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+        }
     }
 }
 
@@ -28,33 +82,21 @@ class GamePanel extends JPanel implements ActionListener {
 
     final int[] x = new int[GAME_UNITS];
     final int[] y = new int[GAME_UNITS];
+    boolean isGameRunning = true;
 
-    boolean isGameRunning = false;
-
-    GamePanel() {
+    GamePanel(Pool pool) {
         this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
-        this.setBackground(Color.black);
+        this.setBackground(Color.green);
         this.setFocusable(true);
-        this.addKeyListener(new MyKeyAdapter());
-        this.addMouseListener(new MyMouseAdapter());
-        this.addMouseMotionListener(new MyMouseMotionAdapter());
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
-    }
-    
-    public class MyKeyAdapter extends KeyAdapter {
-        
     }
 
-    public class MyMouseAdapter extends MouseAdapter {
-        
-    }
-
-    public class MyMouseMotionAdapter extends MouseMotionAdapter {
-        
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawString("Game Started!", 50, 50);
     }
 }
